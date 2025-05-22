13c38a804379f6d259213d1bdd87fb44f172a7b8
GuilhermeLuz041 <guilhermeluzdocarmo01@gmail.com>
2025-05-21 21:35:38 -0300

    1

--- src/tcp/ServerTCP.java
+++ src/tcp/ServerTCP.java
@@ -11,27 +11,25 @@ public class ServerTCP {
             System.out.println("Servidor rodando na porta " + PORT);
 
             while (true) {
-                Socket cliente = servidor.accept();
-                System.out.println("Novo cliente conectado: " + cliente.getInetAddress());
+                Socket client = servidor.accept();
+                System.out.println("Novo cliente conectado: " + client.getInetAddress());
 
                 Thread thread = new Thread(() -> {
                     try (
-                        BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-                        PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true)
+                        BufferedReader entrada = new BufferedReader(new InputStreamReader(client.getInputStream()));
                     ) {
                         String mensagem;
                         while ((mensagem = entrada.readLine()) != null) {
                             System.out.println("Recebido do cliente: " + mensagem);
-                            saida.println(mensagem);
                         }
                     } catch (IOException e) {
-                        System.out.println("Erro com cliente: " + e.getMessage());
+                        System.out.println("Erro: " + e.getMessage());
                     } finally {
                         try {
-                            cliente.close();
+                            client.close();
                             System.out.println("Cliente desconectado.");
                         } catch (IOException e) {
-                            e.getMessage();
+                            e.printStackTrace();
                         }
                     }
                 });
